7cee39a0bddc8b75c9bcad2f1d0a064f0a0e66d8
Mirrochan <m.dryha_fit_4_23_b_d@knute.edu.ua>
2023-12-03 12:58:23 +0200

    firsjfdjf

---
+++ Екзамен/Екзамен.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+void splitIntoPairs(int number) {
+    std::string numberString = std::to_string(number);
+
+    for (int i = 0; i < numberString.length(); ++i) {
+        for (int j = 1; i + j <= numberString.length(); ++j) {
+            std::string pair = numberString.substr(i, j);
+    
+        }
+    }
+}
+
+int main() {
+    int number = 12345;
+    splitIntoPairs(number);
+    std::cout << pair << " ";
+    return 0;
+}
